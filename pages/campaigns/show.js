118b8b0b66df1ee803460c0e92a3ade573aecef8
hmalik88 <hbmalik88@gmail.com>
2020-08-27 16:08:45 -0400

    removed extraneous import statement

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from '../../components/Layout';
 
 export default class CampaignShow extends React.Component {
     render() {
         return(
-            <h3>Campaign show</h3>
+            <Layout>
+                <h3>Campaign show</h3>
+            </Layout>
         )
     }
 }
