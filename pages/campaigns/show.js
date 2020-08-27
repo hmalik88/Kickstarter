5993365313eb2562d57e735184042e77b6bd6b0a
hmalik88 <hbmalik88@gmail.com>
2020-08-27 16:28:28 -0400

    added in getInitialProps function to retrieve summary data from individual contract

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 export default class CampaignShow extends React.Component {
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address);
+        const summary = await campaign.methods.getSummary().call();
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        }
+    }
+
+
     render() {
         return(
             <Layout>
