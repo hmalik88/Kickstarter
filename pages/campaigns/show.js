1e3c618148c6d1d45f73591359bdcc19c3605d22
hmalik88 <hbmalik88@gmail.com>
2020-08-27 17:44:59 -0400

    added in extra grid row to add space between card group view requests button

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
-import { Card, Grid } from 'semantic-ui-react';
+import { Card, Grid, Button } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
 import ContributeForm from '../../components/ContributeForm';
+import { Link } from '../../routes';
 
 export default class CampaignShow extends React.Component {
     static async getInitialProps(props) {
@@ -65,12 +66,24 @@ export default class CampaignShow extends React.Component {
             <Layout>
                 <h3>Campaign show</h3>
                 <Grid>
-                    <Grid.Column width={10}>
-                        {this.renderCards()}
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <ContributeForm address={this.props.address} />
-                    </Grid.Column>
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                            {this.renderCards()}
+                         
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            <ContributeForm address={this.props.address} />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Link route={`/campaigns/${this.props.address}/requests`}>
+                                <a>
+                                    <Button primary>View Requests</Button>
+                                </a> 
+                            </Link>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </Layout>
         )
