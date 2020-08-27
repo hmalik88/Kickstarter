e27fcaa7bd74dd6d4224e262864d776092eb5344
hmalik88 <hbmalik88@gmail.com>
2020-08-26 21:20:10 -0400

    added in css stylesheet and imported in layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import factory from '../ethereum/factory';
+import { Card, Button } from 'semantic-ui-react';
+import Layout from '../components/Layout';
 
 export default class CampaignIndex extends React.Component {
+
     static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
         return { campaigns }
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            }
+        })
+
+        return <Card.Group items={items} />
+    }
+
+
     render() {
-        return (<div>{this.props.campaigns[0]}</div>)
+        return (
+        <div>
+            <Layout>
+            <link async rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"/>
+            <h3>Open Campaigns</h3>
+            {this.renderCampaigns()}
+            <Button 
+                content="Create campaign"
+                icon="add"
+                primary
+            />
+            </Layout>
+        </div>
+            )
     }
 }
