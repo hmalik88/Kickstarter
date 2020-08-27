3657518b8ed1803f0ea1254907c03a184bb88488
hmalik88 <hbmalik88@gmail.com>
2020-08-26 21:33:17 -0400

    added in container to contstrain the width of the content being displayed

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Header from './Header';
 
 
 export default (props) => {
     return(
         <div>
-            <h1>I'm a header</h1>
-            {props.children}
-            <h1>I'm a footer</h1>
+            <Container>
+                <Header/>
+                {props.children}
+                <h1>I'm a footer</h1>
+            </Container>
         </div>
     )
 }
