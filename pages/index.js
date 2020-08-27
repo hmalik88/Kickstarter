1020c49eae5e02eb3d92fe22e199fc078490da5f
hmalik88 <hbmalik88@gmail.com>
2020-08-26 20:29:45 -0400

    added in some basic routes

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import factory from '../ethereum/factory';
+
+export default class CampaignIndex extends React.Component {
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return { campaigns }
+    }
+
+    render() {
+        return (<div>{this.props.campaigns[0]}</div>)
+    }
+}
