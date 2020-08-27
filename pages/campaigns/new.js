274ec5e950cb6502c383e68b2f4a9781df2c0f82
hmalik88 <hbmalik88@gmail.com>
2020-08-27 15:02:03 -0400

    added try/catch block and an error component in the form to display errors

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 export default class CampaignNew extends React.Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
     }
 
     onSubmit = async e => {
         e.preventDefault();
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-            .createCampaign(this.state.minimumContribution)
-            .send({
-                from: accounts[0]
-            });
-    }
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({errorMessage: err.message});
+        }
+    } 
 
     render() {
         return (
             <Layout>
                 <h3>Create a Campaign</h3>
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input 
@@ -33,6 +38,7 @@ export default class CampaignNew extends React.Component {
                             onChange={e => this.setState({minimumContribution: e.target.value})} 
                         />
                     </Form.Field>
+                    <Message error header="Oops!" content={this.state.errorMessage} />
                     <Button primary>Create</Button>
                 </Form>
             </Layout>
